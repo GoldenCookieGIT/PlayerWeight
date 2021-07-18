aebdfe57f31bc109a726fce57d6c08ccedaccdc9
ChaoticWagon <69493011+ChaoticWagon@users.noreply.github.com>
2021-07-18 11:32:34 -0400

    renamed playerweight class to main

--- src/main/java/me/chaoticwagon/playerweight/Main.java
+++ src/main/java/me/chaoticwagon/playerweight/Main.java
@@ -2,7 +2,7 @@ package me.chaoticwagon.playerweight;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class PlayerWeight extends JavaPlugin {
+public final class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
