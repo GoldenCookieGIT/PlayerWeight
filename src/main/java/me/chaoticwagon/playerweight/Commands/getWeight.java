b2d52b44b9d1e8fd6ae1a01aafaf50fe9b786356
ChaoticWagon <69493011+ChaoticWagon@users.noreply.github.com>
2021-07-21 01:14:09 -0400

    /getweight command

--- src/main/java/me/chaoticwagon/playerweight/Commands/getWeight.java
+++ src/main/java/me/chaoticwagon/playerweight/Commands/getWeight.java
@@ -1,5 +1,7 @@
 package me.chaoticwagon.playerweight.Commands;
 
+import me.chaoticwagon.playerweight.Main;
+import me.chaoticwagon.playerweight.Tools;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
@@ -11,6 +13,10 @@ import java.util.logging.Level;
 
 public class getWeight implements CommandExecutor {
 
+    Main plugin;
+    Tools tools;
+
+
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
@@ -21,10 +27,14 @@ public class getWeight implements CommandExecutor {
         Player p = (Player) sender;
 
         if (args.length == 0){
-            p.sendMessage(ChatColor.WHITE + "[" + ChatColor.RED + "PlayerWeight" + ChatColor.WHITE + "] " + "Your weight");
+            p.sendMessage(ChatColor.WHITE + "[" + ChatColor.RED + "PlayerWeight" + ChatColor.WHITE + "] " + "Your weight is " +
+                    ChatColor.GREEN + plugin.currentWeight + "/" + tools.getMaxPlayerWeight(p));
         }
 
-        
+        if (args[0] == null) {
+            p.sendMessage(ChatColor.RED + "Usage: /getweight <PLAYER>");
+            return true;
+        }
 
         return true;
     }
