d4597262c61e6f6115b0c5505fd75eb972dbb730
ChaoticWagon <69493011+ChaoticWagon@users.noreply.github.com>
2021-09-01 17:34:37 -0400

    main

--- src/main/java/me/chaoticwagon/playerweight/Main.java
+++ src/main/java/me/chaoticwagon/playerweight/Main.java
@@ -2,7 +2,9 @@ package me.chaoticwagon.playerweight;
 
 import me.chaoticwagon.playerweight.Commands.Reload;
 import me.chaoticwagon.playerweight.Commands.getWeight;
+import org.bukkit.Server;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitScheduler;
 
 public final class Main extends JavaPlugin {
 
@@ -12,12 +14,17 @@ public final class Main extends JavaPlugin {
         getCommand("playerweight").setExecutor(new Reload(this));
         getCommand("getWeight").setExecutor(new getWeight());
 
+        Server server;
+        BukkitScheduler scheduler = getServer().getScheduler();
+        scheduler.scheduleAsyncRepeatingTask()
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
     }
+
+
 
     public int currentWeight = 0;
 
