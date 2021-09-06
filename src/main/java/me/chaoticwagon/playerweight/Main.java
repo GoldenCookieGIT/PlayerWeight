06c1fa8f260222d2570531d9e12555fcaabfe994
ChaoticWagon <69493011+ChaoticWagon@users.noreply.github.com>
2021-09-06 15:43:43 -0400

    Config Changes

--- src/main/java/me/chaoticwagon/playerweight/Main.java
+++ src/main/java/me/chaoticwagon/playerweight/Main.java
@@ -3,14 +3,11 @@ package me.chaoticwagon.playerweight;
 import me.chaoticwagon.playerweight.Commands.Reload;
 import me.chaoticwagon.playerweight.Commands.getWeight;
 import me.chaoticwagon.playerweight.Listeners.*;
-import org.bukkit.Bukkit;
-import org.bukkit.Material;
-import org.bukkit.Server;
+
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitScheduler;
-import org.bukkit.scheduler.BukkitTask;
 
 import java.util.HashMap;
 import java.util.Map;
