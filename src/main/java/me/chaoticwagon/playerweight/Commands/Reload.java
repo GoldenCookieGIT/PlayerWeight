6e240b54e98fa48a5c73406ea551fd9454c092f1
ChaoticWagon <69493011+ChaoticWagon@users.noreply.github.com>
2021-09-13 11:13:44 -0400

    settings command

--- src/main/java/me/chaoticwagon/playerweight/Commands/Reload.java
+++ src/main/java/me/chaoticwagon/playerweight/Commands/Reload.java
@@ -7,6 +7,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
 
 import java.util.logging.Level;
 
@@ -23,14 +24,28 @@ public class Reload implements CommandExecutor {
 
         if (args.length != 1) {
             if (sender instanceof Player) {
-                sender.sendMessage(ChatColor.RED + "Usage: /playerweight reload");
+                sender.sendMessage(ChatColor.RED + "Usage: /playerweight <>");
                 return true;
             }
-            Bukkit.getLogger().log(Level.SEVERE, "Usage: /playerweight reload");
+            Bukkit.getLogger().log(Level.SEVERE, "Usage: /playerweight <>");
             return true;
         }
 
-        plugin.reloadConfig();
+        if (args[0].equals("reload")){
+            plugin.reloadConfig();
+            return true;
+        }
+
+        if (args[0].equals("settings")) {
+            if (!(sender instanceof Player)) return true;
+            Player p = (Player) sender;
+
+            Inventory settings = Bukkit.createInventory(p,54, ChatColor.RED + "" + ChatColor.BOLD + "Settings");
+            
+
+            p.openInventory(settings);
+            return true;
+        }
 
         return true;
     }
